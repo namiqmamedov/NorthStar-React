5741c4aa2a7be1f5872bfd472a1f41455640a841
namiqmamedov <namigmm@code.edu.az>
2023-05-15 19:46:55 +0400

    Added scroll to top on page change

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,14 @@ import 'bootstrap/dist/css/bootstrap.css'
 import {BrowserRouter as Router} from 'react-router-dom'
 import store from './store/store.jsx'
 import { Provider } from 'react-redux'
+import ScrollToTop from './components/UI/scrollToTop/scrollToTop.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
    <Router>
     <Provider store={store}>
+    <ScrollToTop/>
       <App />
     </Provider>
    </Router>
